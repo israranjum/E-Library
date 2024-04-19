a09661735ef2f84884e08e38daea070bd9ac90c8
israranjum <israranjum851@gmail.com>
2024-04-19 12:17:19 +0500

    Add Error Handling

--- src/user/userController.ts
+++ src/user/userController.ts
@@ -4,6 +4,7 @@ import userModel from "./userModel"
 import bcrypt from "bcrypt"
 import { sign } from "jsonwebtoken"
 import { config } from "../config/config"
+import { User } from "./userTypes"
 
 
 const createUser = async (req: Request, res: Response, next: NextFunction) => {
@@ -15,32 +16,36 @@ const createUser = async (req: Request, res: Response, next: NextFunction) => {
         return next(error)
     }
     // Database Call
-    const user = await userModel.findOne({ email });
-    if (user) {
-        const error = createHttpError(409, "User already exists with this email")
-        return next(error)
+    try {
+        const user = await userModel.findOne({ email });
+        if (user) {
+            const error = createHttpError(409, "User already exists with this email")
+            return next(error)
+        }
+    } catch (error) {
+        return next(createHttpError(500, "Error While Find User"))
     }
 
     // Password hashing
-    const hashedPassword = await bcrypt.hash(password, 10)
 
-    const newUser = await userModel.create({ name, email, password: hashedPassword })
+    const hashedPassword = await bcrypt.hash(password, 10)
+    let newUser: User
+    try {
+        newUser = await userModel.create({ name, email, password: hashedPassword })
+    } catch (error) {
+        return next(createHttpError(500, "Error While Creating User"))
+    }
 
     // Token Generation JWT
-    const token = sign({ sub: newUser._id }, config.jwtSecret as string, { expiresIn: "7d" })
 
+    try {
+        const token = sign({ sub: newUser._id }, config.jwtSecret as string, { expiresIn: "7d" })
+        // Response
+        res.json({ accessToken: token })
+    } catch (error) {
+        return next(createHttpError(500, "Error While Generating Token"))
+    }
 
-
-
-
-
-    // Response
-
-
-
-
-
-    res.json({ accessToken: token })
 }
 
 export { createUser }
