13b62e5624486d6d36cdf8e5f43ddddc1bdf77f0
israranjum <israranjum851@gmail.com>
2024-04-23 10:31:22 +0500

    Add Book Model

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,7 @@
 import express, { NextFunction, Request, Response } from "express";
 import globalErrorHandler from "./middlewares/globalErrorHandler";
 import userRouter from "./user/userRouter";
+import bookRouter from "./book/bookRouter";
 
 
 const app = express();
@@ -17,13 +18,12 @@ app.use(globalErrorHandler)
 
 // Routes
 app.get("/", (req, res) => {
-
-
     res.send("Hello World")
 })
 
 
 app.use("/api/users", userRouter)
+app.use("api/books", bookRouter)
 
 
 
---
+++ src/book/bookRouter.ts
@@ -0,0 +1,10 @@
+import express from "express"
+import { createBook } from "./bookController";
+
+const bookRouter = express.Router()
+
+
+bookRouter.post("/create", createBook);
+
+
+export default bookRouter
